b1e31d74dda817222e22b2e7a5370b7a20bdeb7a
Giovana Oliveira <gigioliveirafju@gmail.com>
2025-03-28 10:23:31 -0300

    SQlite definition

---
+++ Introdução a Banco de Dados/SQLite.js
@@ -0,0 +1,8 @@
+/*
+SQLite, é um banco de dados relacional leve, autosuficiente e totalmente funcional.
+
+Arquivo único: todos os dados, incluindo definições de tabelas, índices e a própria informação armazenada, são mantidos em um único arquivo de disco.
+
+Auto suficiente: o SQlite não depende de nenhuma instalação externa ou configuração de servidor, e pode ser incorporado diretamente em uma aplicação.
+
+*/
